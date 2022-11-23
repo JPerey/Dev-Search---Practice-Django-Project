3e5375ac45b11b88a45dc7b7594430d439d93e94
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-11-23 15:13:19 -0800

    UPDATE: added one-to-many relation with reviews, and many-to-many relation with skill tags and project

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ration = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Review(models.Model):
+
+    VOTE_TYPE = (("UP", "UP VOTE"), ("DOWN", "DOWN VOTE"))
+    # owner=
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    review = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+# one to many - use models.foreignKey in the child table
+# many to many - link model that will be many to the other table that will be many using the models.ManyToManyField
