3db3aad065cc99cebb17adae8a4764ede85f0b69
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-11-23 11:50:10 -0800

    UPDATE: added dynamic pagination using jinja. working on creatig database for website

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        "id": "1",
+        "title": "Ecommerce Website",
+        "description": "Fully functional ecommerce website",
+    },
+    {
+        "id": "2",
+        "title": "Portfolio Website",
+        "description": "A personal website to write articles and display work",
+    },
+    {
+        "id": "3",
+        "title": "Social Network",
+        "description": "An open source project built by the community",
+    },
+]
+
+
 def projects(requests):
-    return render(requests, 'projects.html')
+    msg_plural = "projects"
+    num = 11
+    context = {
+        "message": msg_plural,
+        "num": num,
+        "projects": projectsList,
+    }
+    return render(requests, "projects/projects.html", context)
+
 
 def project(requests, pk):
-    return render(requests, 'single-project.html')
+    projectObj = None
+
+    for i in projectsList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(requests, "projects/single-project.html", {"project": projectObj})
