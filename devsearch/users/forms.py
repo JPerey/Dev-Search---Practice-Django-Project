4487efa82a76536ed66c5ff89fb492c1cb9bc91d
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-12-02 00:59:06 -0800

    UPDATED: added styling to login/ register form page

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -4,16 +4,39 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
-
-    class Meta():
+    class Meta:
         ordering = [-1]
         model = User
-        fields = ['first_name',
-                  'email',
-                  'username',
-                  'password1',
-                  'password2',
-                  ]
+        fields = [
+            "first_name",
+            "email",
+            "username",
+            "password1",
+            "password2",
+        ]
         labels = {
-            'first_name': 'Name',
+            "first_name": "Name",
         }
+
+    def __init__(self, *args: any, **kwargs: any) -> None:
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        self.fields["first_name"].widget.attrs.update(
+            {" class": "input input--text", "id": "formInput#text"}
+        )
+
+        self.fields["email"].widget.attrs.update(
+            {" class": "input input--email", "id": "formInput#email"}
+        )
+
+        self.fields["username"].widget.attrs.update(
+            {" class": "input input--text", "id": "formInput#text"}
+        )
+
+        self.fields["password1"].widget.attrs.update(
+            {" class": "input input--password", "id": "formInput#password"}
+        )
+
+        self.fields["password2"].widget.attrs.update(
+            {" class": "input input--password", "id": "formInput#password"}
+        )
